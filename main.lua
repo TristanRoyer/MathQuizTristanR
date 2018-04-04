6024aaf4524c34ec8938b24e03a3ed21314ab810
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-04-03 20:37:32 -0400

    ok

--- main.lua
+++ main.lua
@@ -40,11 +40,17 @@ local heart4
 
 local incorrectObject
 
-local GameOver = display.newImageRect("Images/gameOver.png",500,500)
- GameOver.isVisible = false
+local GameOver = display.newImageRect("Images/gameOver.png",1200,1400)
+GameOver.x = display.contentCenterX
+GameOver.y = display.contentCenterY
+GameOver.isVisible = false
 
- local YouWin = display.newImageRect("Images/YouWin.jpg",1999,1999)
+ local YouWin = display.newImageRect("Images/YouWin.jpg",1200,1400)
+YouWin.x = display.contentCenterX
+YouWin.y = display.contentCenterY
+
  YouWin.isVisible = false
+
 
 local GameOverSound = audio.loadSound("Sounds/Game Over Sound Effect.mp3")
 local GameOverSoundChannel
@@ -52,6 +58,16 @@ local GameOverSoundChannel
 local FreeMusic = audio.loadSound("Sounds/bensound-acousticbreeze.mp3")
 local FreeMusicChannel
 FreeMusicChannel = audio.play(FreeMusic)
+
+local YouWinSound = audio.loadSound("Sounds/applause3.mp3")
+local YouWinSoundChannel
+
+local CorrectSound = audio.loadSound("Sounds/SOUND EFFECT TADA.mp3")
+local CorrectSoundChannel
+
+local WrongSound = audio.loadSound("Sounds/Aww Sound Effect.mp3")
+local WrongSoundChannel
+
 
    -- create the lives to display on the screen
 heart1 = display.newImageRect("Images/heart.png", 100,100)
@@ -123,6 +139,8 @@ end
      questionObject = display.newText("" , display.contentWidth/3, display.contentHeight/2, nil, 40)
      questionObject:setTextColor(155/255, 0/255, 0/255)
 
+    
+
       clockText = display.newText("TimeLeft:" .. secondsLeft,500,600,nil,40)
     clockText:setTextColor(155/255,180/255,1/255)
 
@@ -144,34 +162,45 @@ end
      
     --create question in text object
     questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+    CorrectText.text = "The correct answer is:" .. correctAnswer
+
     end
 
     if (randomOperation == 2) then correctAnswer = randomNumber1 - randomNumber2
     --create question in text object
     questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
-     
+    CorrectText.text = "The correct answer is:" .. correctAnswer
 end
 
 if ((correctAnswer < 0) and (randomOperation == 2)) then
         correctAnswer = randomNumber2 - randomNumber1
         questionObject.text = randomNumber2 .. " - " .. randomNumber1 .. " = "
+        CorrectText.text = "The correct answer is:" .. correctAnswer
         end
 
 if (randomOperation == 3) then correctAnswer = randomNumber3 * randomNumber4
     --create question in text object
     questionObject.text = randomNumber3 .. " * " .. randomNumber4 .. " = "
+    CorrectText.Text = "The correct answer is:" .. correctAnswer
+
 end
 if (randomOperation == 4) then answerDivision = randomNumber5 / randomNumber6
 correctAnswer = math.ceil(answerDivision)
-
-    -- create the question in text object
+CorrectText.text = "The correct answer is:" .. correctAnswer
+-- create the question in text object
 questionObject.text = randomNumber5 .. " / " .. randomNumber6 .. " = " 
 end
 if (randomOperation == 5) then SquareRoot = math.sqrt(randomNumber7)
     correctAnswer = math.ceil(SquareRoot)
-    questionObject.text = "sqrt of" .. randomNumber7 .. " = "
+    questionObject.text = " sqrt : " .. randomNumber7 .. " = "
+    CorrectText.text = "The correct answer is:" .. correctAnswer
+
 end
 end
+
+ CorrectText = display.newText("The correct answer is:" .. correctAnswer , display.contentWidth/4, display.contentHeight/5, nil, 40)
+     questionObject:setTextColor(155/255, 0/255, 0/255)
+     CorrectText.isVisible = false
 
 
  
@@ -184,6 +213,10 @@ end
 local function HideIncorrect()
     incorrectObject.isVisible = false
     AskQuestion()
+end
+
+local function HideCorrectAnswer()
+  CorrectText.isVisible = false
 end
 
 
@@ -237,6 +270,7 @@ local function NumericFieldListener( event )
                 secondsLeft = totalSeconds
                 score = score + 1
 		        scoreText.text = ("score:" .. score)
+            CorrectSoundChannel = audio.play(CorrectSound)
 
                 else
                 incorrectObject.isVisible = true
@@ -244,6 +278,9 @@ local function NumericFieldListener( event )
                 lives = lives - 1
                 secondsLeft = totalSeconds
                 UpdateHearts()
+                WrongSoundChannel = audio.play(WrongSound)
+                CorrectText.isVisible = true
+                timer.performWithDelay(1000,HideCorrectAnswer)
             end
             if (score == 5) then
                  YouWin.isVisible = true
